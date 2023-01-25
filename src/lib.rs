f22bfc9fdb75a041f32607ae2a5b684a491625f7
Justin de Ruiter <justinderuiter@ziggo.nl>
2023-01-25 22:33:16 +0100

    More refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -399,271 +399,233 @@ pub fn decompress_to_slice<'a>(input: &[u8], output: &'a mut [u8]) -> Result<&'a
         ip += 1;
 
         if t < 4 {
-            next = t;
-            state = next;
-            t = next;
+            state = t;
+        } else {
+            state = 4;
+        }
 
-            if input.len() - ip < t + 3 {
-                return Err(Error::InputOverrun);
-            } else if output.len() - op < t {
-                return Err(Error::Error);
-            } else {
-                while t != 0 {
+        if output.len() - op < t {
+            return Err(Error::Error);
+        }
+
+        if input.len() - ip < t + 3 {
+            return Err(Error::InputOverrun);
+        }
+
+        output[op..op + t].copy_from_slice(&input[ip..ip + t]);
+        op += t;
+        ip += t;
+    }
+
+    let mut current_block;
+
+    loop {
+        t = input[ip] as usize;
+        ip += 1;
+
+        if t < 16 {
+            if state == 0 {
+                if t == 0 {
+                    let ip_last = ip;
+
+                    while input[ip] == 0 {
+                        ip += 1;
+
+                        if input.len() - ip == 0 {
+                            return Err(Error::InputOverrun);
+                        }
+                    }
+
+                    let mut offset = ip - ip_last;
+
+                    if offset > usize::MAX / 255 - 2 {
+                        return Err(Error::Error);
+                    }
+
+                    offset = (offset << 8) - offset;
+                    t += offset + 15 + input[ip] as usize;
+                    ip += 1;
+                }
+
+                t += 3;
+
+                if output.len() - op < t {
+                    return Err(Error::Error);
+                }
+
+                if input.len() - ip < t + 3 {
+                    return Err(Error::InputOverrun);
+                }
+
+                for _ in 0..t {
                     output[op] = input[ip];
                     op += 1;
                     ip += 1;
-                    t -= 1;
                 }
-            }
-        } else if output.len() - op < t {
-            return Err(Error::Error);
-        } else if input.len() - ip < t + 3 {
-            return Err(Error::InputOverrun);
-        } else {
-            loop {
-                output[op] = input[ip];
-                op += 1;
+
+                state = 4;
+
+                continue;
+            } else if state != 4 {
+                next = t & 3;
+                m_pos = op - 1;
+                m_pos -= t >> 2;
+                m_pos -= (input[ip] as usize) << 2;
                 ip += 1;
-                t -= 1;
 
-                if t == 0 {
-                    break;
-                }
-            }
-
-            state = 4;
-        }
-    }
-
-    let mut current_block = 11;
-
-    loop {
-        if current_block == 11 {
-            t = input[ip] as usize;
-            ip += 1;
-
-            if t < 16 {
-                if state == 0 {
-                    if t == 0 {
-                        let ip_last = ip;
-
-                        while input[ip] == 0 {
-                            ip += 1;
-
-                            if input.len() - ip == 0 {
-                                return Err(Error::InputOverrun);
-                            }
-                        }
-
-                        let mut offset = ip - ip_last;
-
-                        if offset > usize::MAX / 255 - 2 {
-                            return Err(Error::Error);
-                        }
-
-                        offset = (offset << 8) - offset;
-                        t += offset + 15 + input[ip] as usize;
-                        ip += 1;
-                    }
-
-                    t += 3;
-
-                    if output.len() - op < t {
-                        return Err(Error::Error);
-                    }
-
-                    if input.len() - ip < t + 3 {
-                        return Err(Error::InputOverrun);
-                    }
-
-                    loop {
-                        output[op] = input[ip];
-                        op += 1;
-                        ip += 1;
-                        t -= 1;
-
-                        if t == 0 {
-                            break;
-                        }
-                    }
-
-                    state = 4;
-                    current_block = 11;
-                    continue;
-                } else if state != 4 {
-                    next = t & 3;
-                    m_pos = op - 1;
-                    m_pos -= t >> 2;
-                    m_pos -= (input[ip] as usize) << 2;
-                    ip += 1;
-
-                    // if m_pos < 0 {
-                    //     current_block = 48;
-                    //     break;
-                    // }
-
-                    if output.len() - op < 2 {
-                        return Err(Error::Error);
-                    }
-
-                    output[op] = output[m_pos];
-                    output[op + 1] = output[m_pos + 1];
-                    op += 2;
-                    current_block = 44;
-                } else {
-                    next = t & 3;
-                    m_pos = op - (1 + 0x800);
-                    m_pos -= t >> 2;
-                    m_pos -= (input[ip] as usize) << 2;
-                    ip += 1;
-                    t = 3;
-                    current_block = 36;
-                }
-            } else {
-                if t >= 64 {
-                    next = t & 3;
-                    m_pos = op - 1;
-                    m_pos -= t >> 2 & 7;
-                    m_pos -= (input[ip] as usize) << 3;
-                    ip += 1;
-                    t = (t >> 5) - 1 + (3 - 1);
-                } else if t >= 32 {
-                    t = (t & 31) + (3 - 1);
-
-                    if t == 2 {
-                        let ip_last = ip;
-
-                        while input[ip] == 0 {
-                            ip += 1;
-
-                            if input.len() - ip < 1 {
-                                return Err(Error::InputOverrun);
-                            }
-                        }
-
-                        let mut offset = ip - ip_last;
-
-                        if offset > usize::MAX / 255 - 2 {
-                            return Err(Error::Error);
-                        }
-
-                        offset = (offset << 8) - offset;
-                        t += offset + 31 + input[ip] as usize;
-                        ip += 1;
-
-                        if input.len() - ip < 2 {
-                            return Err(Error::InputOverrun);
-                        }
-                    }
-
-                    m_pos = op - 1;
-                    next = u16::from_le_bytes(input[ip..ip + 2].try_into().unwrap()) as usize;
-                    ip += 2;
-                    m_pos -= next >> 2;
-                    next &= 3;
-                } else {
-                    m_pos = op;
-                    m_pos -= (t & 8) << 11;
-                    t = (t & 7) + (3 - 1);
-
-                    if t == 2 {
-                        let ip_last = ip;
-
-                        while input[ip] == 0 {
-                            ip += 1;
-
-                            if input.len() - ip == 0 {
-                                return Err(Error::InputOverrun);
-                            }
-                        }
-
-                        let mut offset = ip - ip_last;
-
-                        if offset > usize::MAX / 255 - 2 {
-                            return Err(Error::Error);
-                        }
-
-                        offset = (offset << 8) - offset;
-                        t += offset + 7 + input[ip] as usize;
-                        ip += 1;
-
-                        if input.len() - ip < 2 {
-                            return Err(Error::InputOverrun);
-                        }
-                    }
-
-                    next = u16::from_le_bytes(input[ip..ip + 2].try_into().unwrap()) as usize;
-                    ip += 2;
-                    m_pos -= next >> 2;
-                    next &= 3;
-
-                    if m_pos == op {
-                        if t != 3 {
-                            return Err(Error::Error);
-                        } else if ip == input.len() {
-                            return Ok(&mut output[..op]);
-                        } else if ip < input.len() {
-                            return Err(Error::InputNotConsumed);
-                        } else {
-                            return Err(Error::InputOverrun);
-                        }
-                    }
-
-                    m_pos -= 0x4000;
-                }
-
-                current_block = 36;
-            }
-
-            if current_block == 36 {
                 // if m_pos < 0 {
                 //     current_block = 48;
                 //     break;
                 // }
 
-                let oe = op + t;
-
-                if output.len() - op < t {
+                if output.len() - op < 2 {
                     return Err(Error::Error);
                 }
 
                 output[op] = output[m_pos];
                 output[op + 1] = output[m_pos + 1];
                 op += 2;
-                m_pos += 2;
+                current_block = 44;
+            } else {
+                next = t & 3;
+                m_pos = op - (1 + 0x800);
+                m_pos -= t >> 2;
+                m_pos -= (input[ip] as usize) << 2;
+                ip += 1;
+                t = 3;
+                current_block = 36;
+            }
+        } else {
+            if t >= 64 {
+                next = t & 3;
+                m_pos = op - 1;
+                m_pos -= t >> 2 & 7;
+                m_pos -= (input[ip] as usize) << 3;
+                ip += 1;
+                t = (t >> 5) - 1 + (3 - 1);
+            } else if t >= 32 {
+                t = (t & 31) + (3 - 1);
 
-                loop {
-                    output[op] = output[m_pos];
-                    m_pos += 1;
-                    op += 1;
+                if t == 2 {
+                    let ip_last = ip;
 
-                    if op >= oe {
-                        break;
+                    while input[ip] == 0 {
+                        ip += 1;
+
+                        if input.len() - ip < 1 {
+                            return Err(Error::InputOverrun);
+                        }
+                    }
+
+                    let mut offset = ip - ip_last;
+
+                    if offset > usize::MAX / 255 - 2 {
+                        return Err(Error::Error);
+                    }
+
+                    offset = (offset << 8) - offset;
+                    t += offset + 31 + input[ip] as usize;
+                    ip += 1;
+
+                    if input.len() - ip < 2 {
+                        return Err(Error::InputOverrun);
                     }
                 }
+
+                m_pos = op - 1;
+                next = u16::from_le_bytes(input[ip..ip + 2].try_into().unwrap()) as usize;
+                ip += 2;
+                m_pos -= next >> 2;
+                next &= 3;
+            } else {
+                m_pos = op;
+                m_pos -= (t & 8) << 11;
+                t = (t & 7) + (3 - 1);
+
+                if t == 2 {
+                    let ip_last = ip;
+
+                    while input[ip] == 0 {
+                        ip += 1;
+
+                        if input.len() - ip == 0 {
+                            return Err(Error::InputOverrun);
+                        }
+                    }
+
+                    let mut offset = ip - ip_last;
+
+                    if offset > usize::MAX / 255 - 2 {
+                        return Err(Error::Error);
+                    }
+
+                    offset = (offset << 8) - offset;
+                    t += offset + 7 + input[ip] as usize;
+                    ip += 1;
+
+                    if input.len() - ip < 2 {
+                        return Err(Error::InputOverrun);
+                    }
+                }
+
+                next = u16::from_le_bytes(input[ip..ip + 2].try_into().unwrap()) as usize;
+                ip += 2;
+                m_pos -= next >> 2;
+                next &= 3;
+
+                if m_pos == op {
+                    if t != 3 {
+                        return Err(Error::Error);
+                    }
+
+                    if ip == input.len() {
+                        return Ok(&mut output[..op]);
+                    }
+
+                    if ip < input.len() {
+                        return Err(Error::InputNotConsumed);
+                    }
+
+                    return Err(Error::InputOverrun);
+                }
+
+                m_pos -= 0x4000;
             }
 
-            state = next;
-            t = next;
+            current_block = 36;
+        }
 
-            if input.len() - ip < t + 3 {
-                return Err(Error::InputOverrun);
-            }
+        if current_block == 36 {
+            // if m_pos < 0 {
+            //     current_block = 48;
+            //     break;
+            // }
 
             if output.len() - op < t {
                 return Err(Error::Error);
             }
 
-            while t != 0 {
-                output[op] = input[ip];
+            for _ in 0..t {
+                output[op] = output[m_pos];
+                m_pos += 1;
                 op += 1;
-                ip += 1;
-                t -= 1;
             }
+        }
 
-            current_block = 11;
-        } else {
+        state = next;
+
+        if input.len() - ip < next + 3 {
+            return Err(Error::InputOverrun);
+        }
+
+        if output.len() - op < next {
             return Err(Error::Error);
+        }
+
+        for _ in 0..next {
+            output[op] = input[ip];
+            op += 1;
+            ip += 1;
         }
     }
 }
