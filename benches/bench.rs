178fcd3be887900820ccf387a03105c20cb64c43
Justin de Ruiter <justinderuiter@ziggo.nl>
2023-01-31 19:00:18 +0100

    Remove criterion bench

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,36 +1,29 @@
-// Bench files are taken from the large corpus found in
-// the Canterbury corpus. https://corpus.canterbury.ac.nz/
+use iai::black_box;
 
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use std::fs;
-
-fn bench_compress_and_decompress(c: &mut Criterion, file_name: &str) {
-    let mut g = c.benchmark_group(file_name);
-
-    let data = fs::read(format!("benches/{file_name}")).unwrap();
+fn bench_roundtrip(data: &[u8]) {
     let mut output = vec![0; lzo1x::worst_compress(data.len())];
-
-    g.bench_function("compress", |b| {
-        b.iter(|| {
-            black_box(lzo1x::compress_to_slice(&data, &mut output));
-        })
-    });
-
-    let compressed_data = lzo1x::compress_to_slice(&data, &mut output);
+    let compressed_data = lzo1x::compress_to_slice(data, &mut output);
     let mut output = vec![0; data.len()];
-
-    g.bench_function("decompress", |b| {
-        b.iter(|| {
-            black_box(lzo1x::decompress_to_slice(compressed_data, &mut output).ok());
-        })
-    });
+    lzo1x::decompress_to_slice(compressed_data, &mut output).unwrap();
 }
 
-fn bench(c: &mut Criterion) {
-    bench_compress_and_decompress(c, "bible.txt");
-    bench_compress_and_decompress(c, "E.coli");
-    bench_compress_and_decompress(c, "world192.txt");
+fn bench_roundtrip_bible() {
+    let data = black_box(include_bytes!("bible.txt"));
+    bench_roundtrip(data);
 }
 
-criterion_group!(benches, bench);
-criterion_main!(benches);
+fn bench_roundtrip_ecoli() {
+    let data = black_box(include_bytes!("E.coli"));
+    bench_roundtrip(data);
+}
+
+fn bench_roundtrip_world() {
+    let data = black_box(include_bytes!("world192.txt"));
+    bench_roundtrip(data);
+}
+
+iai::main!(
+    bench_roundtrip_bible,
+    bench_roundtrip_ecoli,
+    bench_roundtrip_world
+);
