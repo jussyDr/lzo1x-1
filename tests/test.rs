6633a5ecfe452c2080face6302fd00f9927f10f7
Justin de Ruiter <justinderuiter@ziggo.nl>
2023-01-31 19:12:27 +0100

    Test for correct length after compression

--- tests/test.rs
+++ tests/test.rs
@@ -4,11 +4,13 @@
 use std::fs;
 use std::path::Path;
 
-fn test_roundtrip(file_name: &str) {
+fn test_roundtrip(file_name: &str, compressed_len: usize) {
     let data = fs::read(Path::new("tests").join(file_name)).unwrap();
 
     let mut output = vec![0; lzo1x::worst_compress(data.len())];
     let compressed_data = lzo1x::compress_to_slice(&data, &mut output);
+
+    assert_eq!(compressed_data.len(), compressed_len);
 
     let mut output = vec![0; data.len()];
     let decompressed_data = lzo1x::decompress_to_slice(compressed_data, &mut output).unwrap();
@@ -18,20 +20,20 @@ fn test_roundtrip(file_name: &str) {
 
 #[test]
 fn roundtrip_a() {
-    test_roundtrip("a.txt");
+    test_roundtrip("a.txt", 5);
 }
 
 #[test]
 fn roundtrip_aaa() {
-    test_roundtrip("aaa.txt");
+    test_roundtrip("aaa.txt", 51269);
 }
 
 #[test]
 fn roundtrip_alphabet() {
-    test_roundtrip("alphabet.txt");
+    test_roundtrip("alphabet.txt", 51294);
 }
 
 #[test]
 fn roundtrip_random() {
-    test_roundtrip("random.txt");
+    test_roundtrip("random.txt", 100397);
 }
