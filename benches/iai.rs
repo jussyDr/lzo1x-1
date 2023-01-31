b05a39793e542918b9133ffe4f8fb5bc23ec517c
Justin de Ruiter <justinderuiter@ziggo.nl>
2023-01-31 16:06:52 +0100

    Add more files to iai bench

--- benches/iai.rs
+++ benches/iai.rs
@@ -1,11 +1,25 @@
 use iai::black_box;
 
-fn roundtrip() {
-    let data = black_box(include_bytes!("bible.txt"));
+fn roundtrip(data: &[u8]) {
     let mut output = vec![0; lzo1x::worst_compress(data.len())];
     let compressed_data = lzo1x::compress_to_slice(data, &mut output);
     let mut output = vec![0; data.len()];
     lzo1x::decompress_to_slice(compressed_data, &mut output).unwrap();
 }
 
-iai::main!(roundtrip);
+fn bible() {
+    let data = black_box(include_bytes!("bible.txt"));
+    roundtrip(data);
+}
+
+fn ecoli() {
+    let data = black_box(include_bytes!("E.coli"));
+    roundtrip(data);
+}
+
+fn world() {
+    let data = black_box(include_bytes!("world192.txt"));
+    roundtrip(data);
+}
+
+iai::main!(bible, ecoli, world);
